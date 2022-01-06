62fdbadf7fb876cf5694f55c95bfa16f33266e03
renatosouzaBR <natoicm@gmail.com>
2022-01-06 10:55:49 -0300

    test: ensure call load method one time

--- src/data/usecases/db-load-deliveries.spec.ts
+++ src/data/usecases/db-load-deliveries.spec.ts
@@ -1,8 +1,48 @@
-class DbLoadDeliveries {}
+import { Delivery } from '@/domain/entities/delivery'
+import { LoadDeliveries } from '@/domain/usecases/load-deliveries'
+
+class DbLoadDeliveries implements LoadDeliveries {
+  constructor(
+    private readonly dbLoadDeliveriesRepository: DbLoadDeliveriesRepository
+  ) {}
+
+  load(identificationIds: [string]): Delivery[] {
+    return this.dbLoadDeliveriesRepository.load(identificationIds)
+  }
+}
+
+interface DbLoadDeliveriesRepository {
+  load(identificationIds: [string]): Delivery[]
+}
+
+class DbLoadDeliveriesRepositoryMock implements DbLoadDeliveriesRepository {
+  callCount = 0
+
+  load(identificationIds: [string]): Delivery[] {
+    this.callCount++
+    return []
+  }
+}
+
+const makeSut = () => {
+  const dbLoadDeliveriesRepositoryMock = new DbLoadDeliveriesRepositoryMock()
+  const sut = new DbLoadDeliveries(dbLoadDeliveriesRepositoryMock)
+
+  return { sut, dbLoadDeliveriesRepositoryMock }
+}
 
 describe('DBLoadDeliveries', () => {
   test('should return definied sut.init', () => {
-    const dbLoadDeliveries = new DbLoadDeliveries()
-    expect(dbLoadDeliveries).toBeDefined()
+    const { sut } = makeSut()
+
+    expect(sut).toBeDefined()
+  })
+
+  test('shoud call load method one time', () => {
+    const { sut, dbLoadDeliveriesRepositoryMock } = makeSut()
+
+    sut.load([''])
+
+    expect(dbLoadDeliveriesRepositoryMock.callCount).toBe(1)
   })
 })

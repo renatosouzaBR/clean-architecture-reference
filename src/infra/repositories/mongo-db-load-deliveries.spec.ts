ca03e816d98ea6a6ac84ca6f886b07f07dc183bb
renatosouzaBR <natoicm@gmail.com>
2022-01-09 19:31:22 -0300

    ensure return a empty list when call load method

--- src/infra/repositories/mongo-db-load-deliveries.spec.ts
+++ src/infra/repositories/mongo-db-load-deliveries.spec.ts
@@ -1,9 +1,24 @@
-class MongoDBLoadDeliveries {}
+import { DbLoadDeliveriesRepository } from '@/data/contracts/db-load-deliveries-repository'
+import { Delivery } from '@/domain/entities/delivery'
+
+class MongoDBLoadDeliveries implements DbLoadDeliveriesRepository {
+  load(identificationIds: string[]): Delivery[] {
+    return []
+  }
+}
 
 describe('MongoDBLoadDeliveries', () => {
   test('should call sut.init sucessfully', () => {
     const mongoDBLoadDeliveries = new MongoDBLoadDeliveries()
 
     expect(mongoDBLoadDeliveries).toBeDefined()
+  })
+
+  test('should return a empty list when call load method', () => {
+    const mongoDBLoadDeliveries = new MongoDBLoadDeliveries()
+
+    const deliveries = mongoDBLoadDeliveries.load([])
+
+    expect(deliveries).toEqual([])
   })
 })

d6c82f8bfedd3bdb37f79a056c7a477cd12003b0
renatosouzaBR <natoicm@gmail.com>
2022-01-14 16:44:53 -0300

    ensure valid if success connetion is true

--- src/main/config/mongoose/mongoose-setup.spec.ts
+++ src/main/config/mongoose/mongoose-setup.spec.ts
@@ -1,9 +1,16 @@
 import { setupMongoose } from '@/main/config/mongoose/mongoose-setup'
+import env from '@/main/config/env'
 
 describe('MongooseSetup', () => {
   test('should throw if connection failure', () => {
     const promise = setupMongoose('')
 
     expect(promise).rejects.toThrow()
+  })
+
+  test('should valid if success connetion is true', async () => {
+    const mongoConnection = await setupMongoose(env.database.url)
+
+    expect(mongoConnection.connection.readyState).toBe(1)
   })
 })
--- src/main/config/mongoose/mongoose-setup.ts
+++ src/main/config/mongoose/mongoose-setup.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
 
 export const setupMongoose = async (mongoURL: string) => {
-  await mongoose.connect(mongoURL)
+  return await mongoose.connect(mongoURL)
 }

3f5f8d68ef2bf020ea3a2e5448d6a47e205de262
renatosouzaBR <natoicm@gmail.com>
2022-01-15 14:42:06 -0300

    add helpers methods to create /deliveries route

--- src/main/express/routes/deliveries-route-setup.ts
+++ src/main/express/routes/deliveries-route-setup.ts
@@ -1,31 +1,12 @@
 import { Router, Express } from 'express'
 
-import { LoadDeliveriesService } from '@/data/services/load-deliveries'
-import { MongoDBLoadDeliveries } from '@/infra/repositories/mongo/load-deliveries'
-import { LoadDeliveriesController } from '@/presentation/controllers/load-deliveries'
+import { makeLoadDeliveriesController } from '@/main/factories/load-deliveries-controller'
+import { adaptRoute } from '@/main/express/route-adapter'
 
 export const setupDeliveriesRoute = (app: Express): void => {
   const router = Router()
 
-  router.get('/', async (req, res) => {
-    const mongoDbLoadDeliveries = new MongoDBLoadDeliveries()
-    const loadDeliveriesService = new LoadDeliveriesService(
-      mongoDbLoadDeliveries
-    )
-    const loadDeliveriesController = new LoadDeliveriesController(
-      loadDeliveriesService
-    )
-
-    const response = await loadDeliveriesController.handle({
-      params: { identificationIds: ['any_ids'] },
-    })
-
-    if (response.type === 'success') {
-      res.send(response)
-    } else {
-      res.status(404).send(response)
-    }
-  })
+  router.get('/', adaptRoute(makeLoadDeliveriesController()))
 
   app.use('/deliveries', router)
 }

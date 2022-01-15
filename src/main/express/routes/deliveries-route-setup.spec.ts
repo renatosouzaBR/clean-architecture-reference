6b8f6e5e29c3b71e11417af2613b8d310c144318
renatosouzaBR <natoicm@gmail.com>
2022-01-15 09:36:58 -0300

    ensure return a list of deliveries

--- src/main/express/routes/deliveries-route-setup.spec.ts
+++ src/main/express/routes/deliveries-route-setup.spec.ts
@@ -3,6 +3,8 @@ import request from 'supertest'
 
 import { setupDeliveriesRoute } from '@/main/express/routes/deliveries-route-setup'
 import { LoadDeliveriesController } from '@/presentation/controllers/load-deliveries'
+import { mockReturnedArray } from '@/helpers/mocks/mock-returned-array'
+import { makeDeliveryFake } from '@/helpers/mocks/mock-delivery'
 
 const makeSut = () => {
   const sut = express()
@@ -33,5 +35,17 @@ describe('DeliveriesRouteSetup', () => {
     const response = await request(sut).get('/deliveries').expect(200)
 
     expect(response.body).toEqual({ data: [], type: 'success' })
+  })
+
+  test('should return a list of deliveries', async () => {
+    const { sut } = makeSut()
+    const deliveries = mockReturnedArray(4, makeDeliveryFake())
+    jest
+      .spyOn(LoadDeliveriesController.prototype, 'handle')
+      .mockResolvedValue({ data: deliveries, type: 'success' })
+
+    const response = await request(sut).get('/deliveries').expect(200)
+
+    expect(response.body).toEqual({ data: deliveries, type: 'success' })
   })
 })

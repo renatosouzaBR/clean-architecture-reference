d54dc6a8e586395421615fe1e6758be4e5c205a5
renatosouzaBR <natoicm@gmail.com>
2022-01-14 23:59:43 -0300

    ensure return a empty list of deliveries

--- src/main/express/routes/deliveries-route-setup.spec.ts
+++ src/main/express/routes/deliveries-route-setup.spec.ts
@@ -4,17 +4,34 @@ import request from 'supertest'
 import { setupDeliveriesRoute } from '@/main/express/routes/deliveries-route-setup'
 import { LoadDeliveriesController } from '@/presentation/controllers/load-deliveries'
 
+const makeSut = () => {
+  const sut = express()
+  setupDeliveriesRoute(sut)
+
+  return { sut }
+}
+
 describe('DeliveriesRouteSetup', () => {
   test('should return a json body in get /deliveries', async () => {
-    const app = express()
-    setupDeliveriesRoute(app)
+    const { sut } = makeSut()
     jest
       .spyOn(LoadDeliveriesController.prototype, 'handle')
       .mockResolvedValue({ data: [], type: 'success' })
 
-    await request(app)
+    await request(sut)
       .get('/deliveries')
       .expect(200)
       .expect('Content-Type', /json/)
+  })
+
+  test('should return a empty list of deliveries', async () => {
+    const { sut } = makeSut()
+    jest
+      .spyOn(LoadDeliveriesController.prototype, 'handle')
+      .mockResolvedValue({ data: [], type: 'success' })
+
+    const response = await request(sut).get('/deliveries').expect(200)
+
+    expect(response.body).toEqual({ data: [], type: 'success' })
   })
 })
